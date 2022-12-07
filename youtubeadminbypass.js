dbc4b5b244a2a835d468dcc60f8fea15af16f49c
ro2alive <119982112+ro2alive@users.noreply.github.com>
2022-12-07 14:23:50 -0600

    Create youtubeadminbypass.js

---
+++ youtubeadminbypass.js
@@ -0,0 +1,2 @@
+// Youtube Admin Bypass //
+javascript:(function () {if (window.location.toString().includes('www.youtube.com/watch?v%27)) { window.open(%27https://www.youtube-nocookie.com/embed/%27 + window.location.toString().split(%27=%27)[1]) }})()
